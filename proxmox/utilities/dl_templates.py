cbd2e1928321292360a71b0523a300f9f54d18ce
funtimes_ninja <funtimes@codewarz.ninja>
2018-10-08 22:59:27 -0400

    beering and coding makes for horrible looking code but it works

---
+++ proxmox/utilities/dl_templates.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+cmd = "pveam available | awk '{print $2}' |grep -v turnkey"
+
+ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+output = ps.communicate()[0]
+output = output.decode("ascii")
+
+final = {}
+
+if __name__ == "__main__":
+    stuff = output.strip().split()
+    for line in sorted(stuff, reverse=True):
+        os_title = line.split('-')[0]
+        if os_title not in final:
+            final[os_title] = line
+
+for shit in final.values():
+    print("Downloading " + shit).strip()
+    cmd = "pveam download local " + shit
+    ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    output = ps.communicate()[0]
+    output = output.decode("ascii")
